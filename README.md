752a46eb29836007bdd6fc4d36fa7325fe58db97
Puyan Wei <puyanwei@gmail.com>
2019-05-27 11:06:53 +0100

    Style changes to page only apply on button click, updated readme

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 This is a chrome extension which will highlight all the same words on a page that a user highlights.
 
 This is a mini project to experience what it is like to make a Chrome extension.
+
+Thanks [@Shiffman](https://github.com/shiffman) for his awesome [Chrome Extension playlist series](https://www.youtube.com/playlist?list=PLRqwX-V7Uu6bL9VOMT65ahNEri9uqLWfS)
--- content.js
+++ content.js
@@ -1,13 +1,15 @@
 console.log("content.js");
 
-let paragraphs = Array.from(document.getElementsByTagName("p"));
-
-paragraphs.forEach(element => {
-    element.style["background-color"] = "red";
-});
-
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
-    console.log(message.txt);
+    console.log("sender :", sender);
+    console.log("sendResponse :", sendResponse());
+    if (message.text === "hello") {
+        let paragraphs = Array.from(document.getElementsByTagName("p"));
+
+        paragraphs.forEach(element => {
+            element.style["background-color"] = "red";
+        });
+    }
 }
